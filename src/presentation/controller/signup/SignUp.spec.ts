2bf224e96442cf211eb3721b44b708097d4dfd19
César Felipe <cesar.felp982@gmail.com>
2021-04-21 19:08:37 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controller/signup/SignUp.spec.ts
+++ src/presentation/controller/signup/SignUp.spec.ts
@@ -52,4 +52,23 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
+
+  test("Should return 400 if no password confirmation is provided", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "César",
+        email: "cesar@gmail.com",
+        password: "123",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation"),
+    );
+  });
 });
