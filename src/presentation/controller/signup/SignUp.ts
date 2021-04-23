617ece732d976f9ee94e7585c01c731062269c1d
César Felipe <cesar.felipe@cvortex.io>
2021-04-23 06:39:22 -0300

    feat: ensure SignUpController return 500 if EmailValidator throws

--- src/presentation/controller/signup/SignUp.ts
+++ src/presentation/controller/signup/SignUp.ts
@@ -3,28 +3,36 @@ import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest } from "../../helpers/http-helper";
 import { EmailValidator } from "../../protocols/email-validator";
 import { InvalidParamError } from "../../errors/invalid-param-error";
+import { ServerError } from "../../errors/server-error";
 
 class SignUpController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
   handle(httpRequest) {
-    const requiredFields = [
-      "name",
-      "email",
-      "password",
-      "passwordConfirmation",
-    ];
+    try {
+      const requiredFields = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email);
+      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email);
 
-    if (!emailIsValid) {
-      return badRequest(new InvalidParamError("email"));
+      if (!emailIsValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+    } catch (e) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
   }
 }
