f4116bbe0ac928983a66c9f2221f212e8fc229b3
César Felipe <cesar.felipe@cvortex.io>
2021-04-28 06:50:58 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter/email-validator-adapter.spec.ts
@@ -1,10 +1,24 @@
 import { EmailValidatorAdapter } from "./email-validator-adapter";
+import validator from "validator";
+
+jest.mock("validator", () => ({
+  isEmail: () => true,
+}));
 
 describe("EmailValidator Adapter", () => {
   test("Should return false if validator return false", () => {
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
+
     const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("invalid_email@gmail.com");
 
     expect(isValid).toBe(false);
+  });
+
+  test("Should return true if validator return true", () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid("valid_email@gmail.com");
+
+    expect(isValid).toBe(true);
   });
 });
