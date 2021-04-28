a2e2a60709b0dcd1890e332c3bf8280e15d0d7fc
César Felipe <cesar.felipe@cvortex.io>
2021-04-28 07:48:11 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account/db-add-account.spec.ts
@@ -104,4 +104,22 @@ describe("DbAddAccount UseCase", () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  test("Should return an account on success", async () => {
+    const { sut } = makeSut();
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+
+    const account = await sut.add(accountData);
+
+    expect(account).toEqual({
+      ...accountData,
+      id: "valid_id",
+      password: "hashed_password",
+    });
+  });
 });
