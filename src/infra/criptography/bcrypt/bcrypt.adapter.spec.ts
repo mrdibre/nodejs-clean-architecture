f3c2a55227df1500d48b9df008ae962c344582d2
César Felipe <cesar.felipe@cvortex.io>
2021-04-28 08:10:14 -0300

    test: ensure BCryptAdapter throws

--- src/infra/criptography/bcrypt/bcrypt.adapter.spec.ts
+++ src/infra/criptography/bcrypt/bcrypt.adapter.spec.ts
@@ -33,4 +33,14 @@ describe("BCrypt Adapter", () => {
 
     expect(hash).toBe("hash");
   });
+
+  test("Should throw if bcrypt throws", async () => {
+    const { sut } = makeSut();
+
+    jest.spyOn(bcrypt, "hash").mockReturnValueOnce(Promise.reject(new Error()));
+
+    const promise = sut.encrypt("any_value");
+
+    await expect(promise).rejects.toThrow();
+  });
 });
