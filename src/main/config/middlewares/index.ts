1b0bf9b795938415d00ec8ca5f707092bc7ec8ed
César Felipe <cesar.felipe@cvortex.io>
2021-04-29 07:28:48 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares/index.ts
+++ src/main/config/middlewares/index.ts
@@ -1,8 +1,11 @@
 import { Express } from "express";
+
+import { cors } from "./cors/cors";
 import { bodyParser } from "./body-parser/body-parser";
 
 const useMiddlewares = (app: Express) => {
   app.use(bodyParser);
+  app.use(cors);
 };
 
 export { useMiddlewares };
