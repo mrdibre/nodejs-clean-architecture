9ffe63c6b91b866e1fad628789551579f6693086
César Felipe <cesar.felipe@cvortex.io>
2021-05-07 07:17:33 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -2,16 +2,23 @@ import { Collection, MongoClient } from "mongodb";
 
 const MongoHelper = {
   client: null as MongoClient,
+  url: null as string,
   async connect(uri: string) {
+    this.url = uri;
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
   },
   async disconnect() {
-    await this?.client?.close();
+    await this.client.close();
+    this.client = null;
   },
   async getCollection(name: string): Promise<Collection<any>> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.url);
+    }
+
     return this.client.db().collection(name);
   },
   mapModelToId(accountData: any): any {
