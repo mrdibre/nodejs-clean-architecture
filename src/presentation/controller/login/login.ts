8de77d0c0f8c5b5eae87caa9542ce6890a68dec3
César Felipe <cesar.felipe@cvortex.io>
2021-05-21 05:58:51 -0300

    feat: ensure LoginControlelr return 400 if no password is provided

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -4,7 +4,11 @@ import { MissingParamError } from "../../errors";
 
 class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError("email"));
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError("email"));
+    } else if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError("password"));
+    }
   }
 }
 
