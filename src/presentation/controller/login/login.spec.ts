b4741f0fe3c2d2e46cf8f5565bc482603b7739c7
César Felipe <cesar.felipe@cvortex.io>
2021-05-21 05:59:35 -0300

    feat: ensure LoginControlelr return 400 if no password is provided

--- src/presentation/controller/login/login.spec.ts
+++ src/presentation/controller/login/login.spec.ts
@@ -24,4 +24,18 @@ describe("Login Controller", () => {
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
   });
+
+  test("Should return 400 if no password is provided", async () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: "any_email",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  });
 });
