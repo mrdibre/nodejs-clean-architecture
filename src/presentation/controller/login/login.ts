7164dc30a3c5d15132004f4f98036dc65b5042c1
César Felipe <cesar.felipe@cvortex.io>
2021-05-21 08:27:12 -0300

    refactor: remove code duplication

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -18,11 +18,14 @@ class LoginController implements Controller {
     try {
       const { email, password } = httpRequest.body;
 
-      if (!email) {
-        return badRequest(new MissingParamError("email"));
-      } else if (!password) {
-        return badRequest(new MissingParamError("password"));
+      const requiredFields = ["email", "password"];
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError("email"));
       }
