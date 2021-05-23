173e58d17a676e2fe033de664b3dabf43bfd50f1
César Felipe <cesar.felp982@gmail.com>
2021-05-23 17:50:19 -0300

    test: ensure CompareFieldValidation works correctly

---
+++ src/presentation/helpers/validators/compare-fields/compare-fields-validation.spec.ts
@@ -0,0 +1,20 @@
+import { CompareFieldsValidation } from "./compare-fields-validation";
+import { InvalidParamError } from "../../../errors";
+
+const makeSut = () => new CompareFieldsValidation("field", "fieldToCompare");
+
+describe("CompareFieldsValidation Validation", () => {
+  test("Should return a InvalidParamError if validation fails", () => {
+    const sut = makeSut();
+    const error = sut.validate({ field: "" });
+
+    expect(error).toEqual(new InvalidParamError("fieldToCompare"));
+  });
+
+  test("Should return void if validation succeeds", () => {
+    const sut = makeSut();
+    const error = sut.validate({ field: "123", fieldToCompare: "123" });
+
+    expect(error).toBeFalsy();
+  });
+});
