be16e8797024b1dd06201ff55c8662a232216b81
César Felipe <cesar.felipe@cvortex.io>
2021-05-24 20:38:41 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success and return null on error

--- src/infra/database/mongodb/account-repository/account-repository.spec.ts
+++ src/infra/database/mongodb/account-repository/account-repository.spec.ts
@@ -1,11 +1,14 @@
 import { MongoHelper } from "../helpers/mongo-helper";
 import { AccountMongoRepository } from "./account-repository";
+import { Collection } from "mongodb";
 
 const makeSut = () => {
   const sut = new AccountMongoRepository();
 
   return { sut };
 };
+
+let accountCollection: Collection;
 
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
@@ -17,12 +20,12 @@ describe("Account Mongo Repository", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("account");
+    accountCollection = await MongoHelper.getCollection("accounts");
 
     await accountCollection.deleteMany({});
   });
 
-  test("Should return an account on success", async () => {
+  test("Should return an account on add success", async () => {
     const { sut } = makeSut();
 
     const account = await sut.add({
@@ -36,5 +39,31 @@ describe("Account Mongo Repository", () => {
     expect(account.name).toBe("any_name");
     expect(account.email).toBe("any_email@gmail.com");
     expect(account.password).toBe("any_password");
+  });
+
+  test("Should return an account on loadByEmail success", async () => {
+    const { sut } = makeSut();
+
+    await accountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@mail.com",
+      password: "any_password",
+    });
+
+    const account = await sut.loadByEmail("any_email@mail.com");
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("any_name");
+    expect(account.email).toBe("any_email@mail.com");
+    expect(account.password).toBe("any_password");
+  });
+
+  test("Should return null if loadByEmail fails", async () => {
+    const { sut } = makeSut();
+
+    const account = await sut.loadByEmail("any_email@mail.com");
+
+    expect(account).toBeFalsy();
   });
 });
--- src/infra/database/mongodb/account-repository/account-repository.ts
+++ src/infra/database/mongodb/account-repository/account-repository.ts
@@ -1,15 +1,26 @@
 import { MongoHelper } from "../helpers/mongo-helper";
 import { AccountModel } from "../../../../domain/models/account";
-import { AddAccountRepository } from "../../../../data/protocols";
 import { AddAccountModel } from "../../../../domain/usecases/account/add-account";
+import {
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
+} from "../../../../data/protocols";
 
-class AccountMongoRepository implements AddAccountRepository {
+class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection("accounts");
 
     const { ops } = await accountCollection.insertOne(accountData);
 
     return MongoHelper.mapModelToId(ops[0]);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({ email });
+
+    return account ? MongoHelper.mapModelToId(account) : null;
   }
 }
 
