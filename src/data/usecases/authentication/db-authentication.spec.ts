b15f27175266a45d8bfbff9f7e2b47acdf604901
César Felipe <cesar.felipe@cvortex.io>
2021-05-24 13:14:49 -0300

    test: ensure DbAuthentication throws if UpdateAccessTokenRepository throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -176,4 +176,16 @@ describe("DbAuthentication UseCase", () => {
 
     expect(updateSpy).toHaveBeenCalledWith("any_id", "any_token");
   });
+
+  test("Should throw if UpdateAccessTokenRepository throws", async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(updateAccessTokenRepositoryStub, "update")
+      .mockReturnValueOnce(new Promise((_, reject) => reject(new Error())));
+
+    const promise = sut.auth(makeFakeAuthentication());
+
+    await expect(promise).rejects.toThrow();
+  });
 });
