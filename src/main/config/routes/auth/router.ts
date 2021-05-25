dc81170b5e9327bc33f951079e7fa9a3c07fb6b5
César Felipe <cesar.felipe@cvortex.io>
2021-05-25 14:48:04 -0300

    feat: adding tsc build

--- src/main/config/routes/auth/router.ts
+++ src/main/config/routes/auth/router.ts
@@ -3,9 +3,9 @@ import { adaptExpressRoute } from "../../../adapters/express/adapt-express-route
 import { makeSignUpController } from "../../../factories/signup/signup";
 import { makeLoginController } from "../../../factories/login/login-factory";
 
-const registerRoute = (router: Router) => {
+const registerAuthRoutes = (router: Router) => {
   router.post("/signup", adaptExpressRoute(makeSignUpController()));
   router.post("/login", adaptExpressRoute(makeLoginController()));
 };
 
-export { registerRoute };
+export { registerAuthRoutes };
--- src/main/config/routes/index.ts
+++ src/main/config/routes/index.ts
@@ -1,16 +1,12 @@
 import { Express, Router } from "express";
-import fg from "fast-glob";
+import { registerAuthRoutes } from "./auth/router";
 
 const useRoutes = (app: Express) => {
   const router = Router();
 
   app.use("/api", router);
 
-  fg.sync("**/src/main/config/routes/**/router.ts").map(async file => {
-    const { registerRoute } = await import(`../../../../${file}`);
-
-    registerRoute(router);
-  });
+  registerAuthRoutes(router);
 };
 
 export { useRoutes };
