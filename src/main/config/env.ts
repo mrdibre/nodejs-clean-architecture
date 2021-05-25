914f737e57c6ca16a6dcc6f26673c768da228163
César Felipe <cesar.felipe@cvortex.io>
2021-05-24 21:10:59 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,7 @@
 const Env = {
   mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api",
   port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || "tj670==5H",
 };
 
 export default Env;
