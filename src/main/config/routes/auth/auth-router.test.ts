d7a983c41b0ccc2de38ff0eed34946204d8646a1
César Felipe <cesar.felipe@cvortex.io>
2021-06-01 08:41:23 -0300

    test: fixing integration test

--- src/main/config/routes/auth/auth-router.test.ts
+++ src/main/config/routes/auth/auth-router.test.ts
@@ -17,7 +17,7 @@ describe("Auth Routes", () => {
   });
 
   beforeEach(async () => {
-    accountCollection = await MongoHelper.getCollection("account");
+    accountCollection = await MongoHelper.getCollection("accounts");
 
     await accountCollection.deleteMany({});
   });
