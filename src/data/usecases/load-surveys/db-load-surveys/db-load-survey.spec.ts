bbe33cef66a0b9b36fedf25fbef616cedfcf9505
César Felipe <cesar.felipe@cvortex.io>
2021-06-02 19:07:59 -0300

    feat: ensure DBLoadSurveys returns a list of serveys on success

--- src/data/usecases/load-surveys/db-load-surveys/db-load-survey.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys/db-load-survey.spec.ts
@@ -2,10 +2,35 @@ import { DbLoadSurveys } from "./db-load-surveys";
 import { SurveyModel } from "../../../../domain/models/survey";
 import { LoadSurveysRepository } from "../../../protocols/database/survey/load-surveys-repository";
 
+const makeFakeSurveys = (): SurveyModel[] => [
+  {
+    id: "any_id",
+    question: "any_question",
+    date: new Date(),
+    answers: [
+      {
+        image: "any_image",
+        answer: "any_answer",
+      },
+    ],
+  },
+  {
+    id: "other_id",
+    question: "other_question",
+    date: new Date(),
+    answers: [
+      {
+        image: "other_image",
+        answer: "other_answer",
+      },
+    ],
+  },
+];
+
 const makeLoadSurveyRepositoryStub = () => {
   class LoadSurveysRepositoryStub implements LoadSurveysRepository {
     async loadAll(): Promise<SurveyModel[]> {
-      return [];
+      return makeFakeSurveys();
     }
   }
 
@@ -32,6 +57,14 @@ describe("DbLoadSurveys UseCase", () => {
     await sut.load();
 
     expect(loadAllSpy).toHaveBeenCalled();
+  });
+
+  test("Should return a list of Surveys on success", async () => {
+    const { sut } = makeSut();
+
+    const surveys = await sut.load();
+
+    expect(surveys).toEqual(makeFakeSurveys());
   });
 
   test("Should throw if AddSurveyRepository throws", async () => {
