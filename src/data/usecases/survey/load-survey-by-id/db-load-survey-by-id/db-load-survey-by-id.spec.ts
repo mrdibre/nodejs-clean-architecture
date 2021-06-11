5573b4939681f12782b468e8971d24efb4552559
César Felipe <cesar.felp982@gmail.com>
2021-06-11 19:30:57 -0300

    test: ensure DbLoadSurveyById throws if LoadSurveyByIdRepository throws

--- src/data/usecases/survey/load-survey-by-id/db-load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/survey/load-survey-by-id/db-load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -62,4 +62,16 @@ describe("DbLoadSurveyById UseCase", () => {
 
     expect(surveys).toEqual(makeFakeSurveys());
   });
+
+  test("Should throw if LoadSurveyByIdRepository throws", async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(loadSurveyByIdRepositoryStub, "loadById")
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    const promise = sut.loadById("any_id");
+
+    await expect(promise).rejects.toThrow();
+  });
 });
